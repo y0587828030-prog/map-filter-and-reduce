dda528d674204bfd13e1d264a911bbcb0678ff7f
yehoshua <y0587828030@gmail.com>
2026-07-05 15:44:13 +0300

    step 2

--- filter.py.py
+++ filter.py.py
@@ -1,3 +1,9 @@
-numbers = [3, 7, 10, 15]
-res = map(lambda num: num + 10 , numbers)
+# ## step 1
+# numbers = [3, 7, 10, 15]
+# res = map(lambda num: num + 10 , numbers)
+# print(list(res))
+
+## step 2
+prices = [100, 50, 200, 80]
+res = map(lambda num: num * 1.17 , prices)
 print(list(res))
